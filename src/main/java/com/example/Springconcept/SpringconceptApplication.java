f395a2d7f3703b797e88e6f99eda7fdd53faa4c5
Amrrish1209 <roshanbabubl@gmail.com>
2023-06-09 11:46:16 +0530

    Add: Using Employee Bean and Department Bean, demonstrate Dependency Injection

--- src/main/java/com/example/Springconcept/SpringconceptApplication.java
+++ src/main/java/com/example/Springconcept/SpringconceptApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.example.Springconcept.component.DemoBean;
+import com.example.Springconcept.component.EmployeeBean;
 
 @SpringBootApplication
 public class SpringconceptApplication {
@@ -16,6 +17,11 @@ public class SpringconceptApplication {
 		logger.debug("Welcome to Spring Concept");
 		ApplicationContext context=SpringApplication.run(SpringconceptApplication.class, args);
 		logger.debug("Checking Context: ",context.getBean(DemoBean.class));
+		logger.debug("\n*** Example using @Autowire annotation on property ***");
+		EmployeeBean employeeBean=context.getBean(EmployeeBean.class);
+		employeeBean.setEid(104);
+		employeeBean.setEname("Spring framework Guru");
+		employeeBean.showEmployeeDetails();
 	}
 
 }
